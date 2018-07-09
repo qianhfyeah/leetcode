da6cd2deba9a02a3dedc3add624584e81009410d
Donald Qian <donald.qian@zoom.us>
2018-07-09 14:50:08 +0800

    reverse integer commit

---
+++ 201807/reverse.c
@@ -0,0 +1,38 @@
+//
+//
+//
+//Given a 32-bit signed integer, reverse digits of an integer.
+
+#include <math.h>
+#include <limits.h>
+
+
+int reverse(int x) {
+    long long result = 0;
+    while(x) {
+        result = result * 10 + x%10;
+        x /= 10;
+    }
+
+    if (result > INT_MAX || result < INT_MIN)
+        return 0;
+
+    return (int)result;
+}
+
+
+bool test() {
+    // 123 expect 321
+    do {
+        if (reverse(1563847412) != 0)
+            break;
+        if (reverse(-123) != -321)
+            break;
+        if (reverse(1200) != 21)
+            break;
+        
+        return true;
+    } while(0);
+
+    return false;
+}
---
+++ main.cpp
@@ -0,0 +1,31 @@
+//
+//
+//
+
+#include <stdio.h>
+
+//bool test();
+
+#include "reverse.c"
+
+
+void test_sink() 
+{
+    //
+    printf("begin test...\r\n");
+
+    if (test()) {
+        printf("test success!!!\r\n");
+    }
+    else {
+        printf("bad test...\r\n");
+    }
+
+    printf("end test...\r\n");
+}
+
+int main() 
+{
+    test_sink();
+    return 0;
+}
